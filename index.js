180cc39c937750f2507d6560e330dcef213d4cf8
Rustam Kasad <rustamkasad@gmail.com>
2020-11-29 20:42:08 -0800

    Refactor to show an ASCII single-string with a given mode.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Mode } = require('@tonaljs/tonal');
+const { Mode, Scale, Interval } = require('@tonaljs/tonal');
+const _ = require('lodash');
 
-Mode.all().forEach(mode => console.log(mode.name, mode.modeNum));
+// Mode.all().forEach(mode => console.log(mode.name, mode.modeNum));
 
 function pickStringAndMode() {
   const randomStringSeed = Math.random();
@@ -12,14 +13,52 @@ function pickStringAndMode() {
   const modeIndex = Math.round(randomStringSeed * 7);
   const selectedMode = Mode.all()[modeIndex];
 
-  console.log(modeIndex, Mode.all()[modeIndex - 1]);
+  // console.log(modeIndex, Mode.all()[modeIndex - 1]);
   // console.log(randomStringSeed, stringIndex);
-  console.log('Play ', strings[stringIndex], selectedMode.name, "(", selectedMode.modeNum, ")");
+  return { 
+    string: strings[stringIndex], 
+    mode: selectedMode.name
+  };
+  // console.log(`Now play:
+  //   string=${strings[stringIndex]} 
+  //   mode=${selectedMode.name} (${selectedMode.modeNum})
+  // `);
 }
 
 // kick it off!
-pickStringAndMode();
-setInterval(pickStringAndMode, 60000);
+const thingie = pickStringAndMode();
+console.log("THINGIE: ", thingie);
 
-// process.exit(0);
+const scale = Scale.get("D dorian"); // =>["C", "D", "E", "F", "G", "A", "B"];
+// console.log(scale);
+ 
+const notes = scale.notes;
 
+const noteIndexes = _.range(0, 6);
+const intervals = noteIndexes
+  .map(idx => ({ a: notes[idx], b: notes[idx + 1] }))
+  .map(obj => Interval.distance(obj.a, obj.b))
+  .map(interval => interval === '2M' ? 'whole step' : 'half step');
+
+console.log('Intervals: ', intervals);
+const vizuel = intervals.map(step => step === 'whole step' ? " * |   |" : " *  |");
+console.log(vizuel.join(''));
+
+intervals.forEach(i => {
+  if (i === 'whole step') {
+    console.log("o");
+    console.log("|");
+  } else {
+    console.log("o");
+  }
+});
+console.log('o');
+
+// console.log(Interval.distance('C', 'D'));
+// console.log(Interval.distance('D', 'E'));
+// console.log(Interval.distance('E', 'F'));
+// console.log(Interval.distance('F', 'G'));
+
+// setInterval(pickStringAndMode, 5000);
+
+process.exit(0);
